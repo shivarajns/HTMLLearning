4ad8794b865e78fa4d1d2346012e2ff2fbcbd865
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-07-31 21:20:23 +0530

    An input default value is given and debug the some of bugs

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <p>Selected Color<div>{color}</div></p>
         </div>
       <label>Select a color</label>
-      <input type='color' onChange={handleColor}></input>
+      <input type='color' defaultValue={"#FFFFFF"} onChange={handleColor}></input>
       <p>Hex Value of color : {color}</p>
       </div>
     </>
