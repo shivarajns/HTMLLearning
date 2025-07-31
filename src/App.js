8a6b94de0ffce387a32b4ab4a2d4d632c11cb7fb
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-07-31 21:00:35 +0530

    Simple Color picker project using React.js which display Hex value of selected color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React, {useState} from 'react'
 
 
 function App() {
+  
+  const[color, setColor] = useState('#FFFFFF');
+  
+  let handleColor = (e)=> {
+    setColor(e.target.value);
+  }
+  
   return (
     <>
-      <h1>Welcome</h1>
+      <div className="color-picker-cnt">
+        <h2>Color Picker</h2>
+        <div className='color-cont' style={{backgroundColor: color}}>
+          <p>Selected Color<div>{color}</div></p>
+        </div>
+      <label>Select a color</label>
+      <input type='color' onChange={handleColor}></input>
+      <p>Hex Value of color : {color}</p>
+      </div>
     </>
   );
 }
